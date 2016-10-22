92004332466616b58e877e6d4c9d712b09375949
Lars Christensen <larsch@belunktum.dk>
2016-10-22 18:41:17 +0000

    Reduce z-axis acceleration to minimize vibration

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -762,7 +762,7 @@
 
 #define DEFAULT_AXIS_STEPS_PER_UNIT   {100,100,400,95.4}  // default steps per unit for Ultimaker
 #define DEFAULT_MAX_FEEDRATE          {400, 400, 30, 25}    // (mm/sec)
-#define DEFAULT_MAX_ACCELERATION      {6000,6000,100,10000}    // X, Y, Z, E maximum start speed for accelerated moves. E default values are good for Skeinforge 40+, for older versions raise them a lot.
+#define DEFAULT_MAX_ACCELERATION      {6000,6000,40,10000}    // X, Y, Z, E maximum start speed for accelerated moves. E default values are good for Skeinforge 40+, for older versions raise them a lot.
 
 #define DEFAULT_ACCELERATION          3000    // X, Y, Z and E acceleration in mm/s^2 for printing moves
 #define DEFAULT_RETRACT_ACCELERATION  3000    // E acceleration in mm/s^2 for retracts
