beb6a97fb929317ac5e3a68409b9ab493d24d2ac
Lars Christensen <larsch@belunktum.dk>
2016-12-03 11:19:57 +0100

    Reduce extruder fan speed by 25%

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -213,7 +213,7 @@
 #define EXTRUDER_2_AUTO_FAN_PIN -1
 #define EXTRUDER_3_AUTO_FAN_PIN -1
 #define EXTRUDER_AUTO_FAN_TEMPERATURE 50
-#define EXTRUDER_AUTO_FAN_SPEED 255 // = full speed
+#define EXTRUDER_AUTO_FAN_SPEED 192 // = full speed
 
 //===========================================================================
 //============================ Mechanical Settings ==========================
